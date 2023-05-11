9aa7a98c8ff7cf62224782f5b3edc92ebe144fb4
Chingiz Olzhabaev <olzhabaev.sh@outlook.com>
2023-05-12 00:56:13 +0200

    update styles

--- source/Diol/src/applications/DiolVSIX/DiolToolWindow.cs
+++ source/Diol/src/applications/DiolVSIX/DiolToolWindow.cs
@@ -23,7 +23,7 @@ namespace DiolVSIX
         /// </summary>
         public DiolToolWindow() : base(null)
         {
-            this.Caption = "DiolToolWindow";
+            this.Caption = "Diol";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
