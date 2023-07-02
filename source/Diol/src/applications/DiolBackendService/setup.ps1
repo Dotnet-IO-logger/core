c1e4636b40f249a34be233daf4466703ff592cde
Chingiz Olzhabaev <olzhabaev.sh@outlook.com>
2023-07-02 21:54:47 +0200

    update script

--- source/Diol/src/applications/DiolBackendService/setup.ps1
+++ source/Diol/src/applications/DiolBackendService/setup.ps1
@@ -1,18 +1,22 @@
-﻿$serviceName = 'DiolBackendService'
+﻿# service information
+$serviceName = 'DiolBackendService'
 $serviceDisplayName = 'Diol Backend Service'
 $exeName = 'DiolBackendService.exe'
 
+# current user. <domain>/<username>
 $username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 
+# service url
 $url = "http://localhost:62023/"
 
+# exe directory
 $currentDir = (Get-Item .).FullName
-
 $exePath = $currentDir + '\' + $exeName
 
 # check if the service exist
 $service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
 
+# we stop and remove the service if it exists
 if ($service.Length -gt 0) {
 	Write-Host "Service $serviceName already exists. We are removing it" -ForegroundColor Yellow
 	Stop-Service -Name $serviceName
@@ -20,6 +24,7 @@ if ($service.Length -gt 0) {
 	Write-Host "Service $serviceName ahs removed" -ForegroundColor Green
 }
 
+# create the service
 Write-Host "Creating service $serviceName" -ForegroundColor Yellow
 
 # before start we also need to provide a user credentials for the service
