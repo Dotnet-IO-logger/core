18875444a87faf88dbbb841c3bcf6e4824cdcf97
Sergey Antonov (from Dev Box) <santonov@microsoft.com>
2024-08-12 14:38:34 +0200

    Symplify code

--- source/Diol/src/Diol.Share/Services/SqlQueryService.cs
+++ source/Diol/src/Diol.Share/Services/SqlQueryService.cs
@@ -21,72 +21,32 @@ namespace Diol.Share.Services
         /// </summary>
         /// <param name="sqlQuery">The SELECT query.</param>
         /// <returns>The table name.</returns>
-        public static string ExtractTableNameFromSelectQuery(string sqlQuery)
-        {
-            string pattern = @"FROM\s+\[?(\w+)\]?";
-            Match match = Regex.Match(sqlQuery, pattern, RegexOptions.IgnoreCase);
-
-            if (match.Success)
-            {
-                return match.Groups[1].Value;
-            }
-
-            return string.Empty;
-        }
+        public static string ExtractTableNameFromSelectQuery(string sqlQuery) =>
+            ExtractFromQuery(sqlQuery, @"FROM\s+\[?(\w+)\]?");
 
         /// <summary>
         /// Extracts the table name from an INSERT query.
         /// </summary>
         /// <param name="sqlQuery">The INSERT query.</param>
         /// <returns>The table name.</returns>
-        public static string ExtractTableNameFromInsertQuery(string sqlQuery)
-        {
-            string pattern = @"INTO\s+\[?(\w+)\]?";
-            Match match = Regex.Match(sqlQuery, pattern, RegexOptions.IgnoreCase);
-
-            if (match.Success)
-            {
-                return match.Groups[1].Value;
-            }
-
-            return string.Empty;
-        }
+        public static string ExtractTableNameFromInsertQuery(string sqlQuery) =>
+            ExtractFromQuery(sqlQuery, @"INTO\s+\[?(\w+)\]?");
 
         /// <summary>
         /// Extracts the table name from an UPDATE query.
         /// </summary>
         /// <param name="sqlQuery">The UPDATE query.</param>
         /// <returns>The table name.</returns>
-        public static string ExtractTableNameFromUpdateQuery(string sqlQuery)
-        {
-            string pattern = @"UPDATE\s+\[?(\w+)\]?";
-            Match match = Regex.Match(sqlQuery, pattern, RegexOptions.IgnoreCase);
-
-            if (match.Success)
-            {
-                return match.Groups[1].Value;
-            }
-
-            return string.Empty;
-        }
+        public static string ExtractTableNameFromUpdateQuery(string sqlQuery) =>
+            ExtractFromQuery(sqlQuery, @"UPDATE\s+\[?(\w+)\]?");
 
         /// <summary>
         /// Extracts the table name from a DELETE query.
         /// </summary>
         /// <param name="sqlQuery">The DELETE query.</param>
         /// <returns>The table name.</returns>
-        public static string ExtractTableNameFromDeleteQuery(string sqlQuery)
-        {
-            string pattern = @"FROM\s+\[?(\w+)\]?";
-            Match match = Regex.Match(sqlQuery, pattern, RegexOptions.IgnoreCase);
-
-            if (match.Success)
-            {
-                return match.Groups[1].Value;
-            }
-
-            return string.Empty;
-        }
+        public static string ExtractTableNameFromDeleteQuery(string sqlQuery) =>
+            ExtractFromQuery(sqlQuery, @"FROM\s+\[?(\w+)\]?");
 
         /// <summary>
         /// Extracts the operation name from a SQL query.
@@ -116,6 +76,24 @@ namespace Diol.Share.Services
         public static bool IsTransaction(string sqlQuery) 
         {
             return sqlQuery.Contains("IMPLICIT_TRANSACTIONS");
+        }
+        
+        /// <summary>
+        /// Extracts a value from a SQL query base on pattern.
+        /// </summary>
+        /// <param name="sqlQuery">The query.</param>
+        /// <param name="pattern">Regex pattern.</param>
+        /// <returns></returns>
+        private static string ExtractFromQuery(string sqlQuery, string pattern)
+        {
+            Match match = Regex.Match(sqlQuery, pattern, RegexOptions.IgnoreCase);
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+
+            return string.Empty;
         }
     }
 }
