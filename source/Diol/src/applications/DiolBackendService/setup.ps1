39a7cb39ed1c188bf9ff57718d46aba1db6a8dd9
Chingiz Olzhabaev <olzhabaev.sh@outlook.com>
2024-08-14 19:07:30 +0200

    Setup release pipeline for DiolBackendService

--- source/Diol/src/applications/DiolBackendService/setup.ps1
+++ source/Diol/src/applications/DiolBackendService/setup.ps1
@@ -1,38 +1,16 @@
-﻿# service information
-$serviceName = 'DiolBackendService'
-$serviceDisplayName = 'Diol Backend Service'
-$exeName = 'DiolBackendService.exe'
+﻿$packageName = 'DiolBackendService'
+$packagePath = '.\distrib\release\DiolBackendService'
 
-# current user. <domain>/<username>
-$username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$csProjPath = '.\DiolBackendService.csproj'
 
-# service url
-$url = "http://localhost:62023/"
+# delete old one
+dotnet tool uninstall -g $packageName
 
-# exe directory
-$currentDir = (Get-Item .).FullName
-$exePath = $currentDir + '\' + $exeName
+# pack and install
+dotnet build -c Release
+dotnet pack $csProjPath --output $packagePath
+dotnet tool install --global --add-source $packagePath $packageName
 
-# check if the service exist
-$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
-
-# we stop and remove the service if it exists
-if ($service.Length -gt 0) {
-	Write-Host "Service $serviceName already exists. We are removing it" -ForegroundColor Yellow
-	Stop-Service -Name $serviceName
-	sc.exe delete $serviceName
-	Write-Host "Service $serviceName ahs removed" -ForegroundColor Green
-}
-
-# create the service
-Write-Host "Creating service $serviceName" -ForegroundColor Yellow
-
-# before start we also need to provide a user credentials for the service
-# user name should contains domain name (sometimes it is the same as computer name)
-New-Service -Name $serviceName -BinaryPathName "$exePath --urls=$url" -DisplayName $serviceDisplayName -StartupType Automatic -Credential $username
-
-Write-Host "$serviceName has installed" -ForegroundColor Green
-
-Start-Service -Name $serviceName
-
-Write-Host "$serviceName has started" -ForegroundColor Green
+# how to use
+# from a terminal please run the following command:
+# DiolBackendService --urls=http://localhost:62023/
